2c2d63d01f958972b5bf9d1607b7e25e94a05ac0
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-09-21 11:05:24 +0300

    rewriting code

---
+++ ava.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+def record_audio():
+     with sr.Microphone() as source:
+          audio=r.listen(source)
+          voice_data=''
+          try:
+               voice_data=r.recognize_google(audio)
+          except sr.UnknownValueError:
+               print("Sorry, I did not get that")
+          except sr.RequestError:
+               print('Sorry, my speech service is down')
+          return voice_data
+
+print('My name is Googles, How can I help you?')
+voice_data=record_audio()
